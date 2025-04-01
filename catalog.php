86bf59cb09e9e555757ce975090f7e9901992d0e
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-01 17:02:16 +0100

    Made changes to the footer

--- catalog.php
+++ catalog.php
@@ -200,8 +200,8 @@ include_once "comp/header.php"
                         // Get the thumbnail image
                         $prodsql_img_thumbnail = mysqli_query($conn, "SELECT * FROM product_images WHERE product_id = '$product_id' AND thumbnail = 1");
                         $row_prod_img_thumbnail = mysqli_fetch_assoc($prodsql_img_thumbnail);
-                        $image_path_thumbnail = $row_prod_img_thumbnail['image_path'];
-                        $product_img = 'admin/'.$image_path_thumbnail;
+                        $image_path_thumbnail = 'admin/'.$row_prod_img_thumbnail['image_path'];
+                        $product_img = $image_path_thumbnail;
 
                         // Get the non-thumbnail images
                         $prodsql_img = mysqli_query($conn, "SELECT * FROM product_images WHERE product_id = '$product_id' AND thumbnail = 0");
