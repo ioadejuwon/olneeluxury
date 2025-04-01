cccfc4974a153c6b64163a17554dcd554f678b81
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-01 12:25:02 +0100

    Made changes to the footer

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,8 @@
 <?php
 // include_once "../inc/auth.php"; 
 $pagetitle = "Login";
-
+include_once "../inc/config.php";
+include_once "../inc/drc.php";
 include_once "ad_comp/adm-head.php";
 
 // include_once "header.php"; 
