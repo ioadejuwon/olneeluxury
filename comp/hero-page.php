1d5b19acf58ead909190423bec97b2ae49a63051
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-01 09:01:35 +0100

    Olnee Luxury Website Initiation

--- comp/hero-page.php
+++ comp/hero-page.php
@@ -1,34 +1,114 @@
-<section data-anim-wrap class="masthed hero-page -type-">
-    <div class="masthead__bg"></div>
+<section data-anim-wrap class="masthed hero-page -type- d-none">
+  <div class="masthead__bg"></div>
 
-    <div class="masthead__container">
-      <div class="row y-gap0 items-center">
-        <div class="col-lg-6">
-          <div class="masthead__content left">
-            <div data-anim-child="slide-up delay-2" class="text-17 lh-15 text-deep-green-1 fw-500 mb-10 subtxt">
-              ELEGANCE || QUALITY || AFFORDABLE
-            </div>
-            <h1 data-anim-child="slide-up delay-2" class="masthead__title">
-              Effortless Elegance
-            </h1>
-            <p data-anim-child="slide-up delay-3" class="mt-20">
-              Premium Quality products that will keep you <br class="lg:d-none">cool and comfortable all day long.
-            </p>
-            <div data-anim-child="slide-up delay-4" class="row items-center x-gap-20 y-gap-20 pt-20">
-              <div class="col-auto cta">
-                <a href="<?php echo SHOP ?>" class="button -md  bg-white  text-dark">Check Catalog</a>
-              </div>
-            </div>
-
-
+  <div class="masthead__container">
+    <div class="row y-gap0 items-center">
+      <div class="col-lg-6">
+        <div class="masthead__content left">
+          <div data-anim-child="slide-up delay-2" class="text-17 lh-15 text-deep-green-1 fw-500 mb-10 subtxt">
+            SENATOR MATERIAL | MEN’S FABRIC STORE IN LAGOS
           </div>
+          <h1 data-anim-child="slide-up delay-2" class="masthead__title">
+            Effortless Elegance
+          </h1>
+          <p data-anim-child="slide-up delay-3" class="mt-20">
+            Premium Quality products that will keep you <br class="lg:d-none">cool and comfortable all day long.
+          </p>
+          <div data-anim-child="slide-up delay-4" class="row items-center x-gap-20 y-gap-20 pt-20">
+            <div class="col-auto cta">
+              <a href="<?php echo SHOP ?>" class="button -md  bg-white  text-dark">Check Catalog</a>
+            </div>
+          </div>
+
+
         </div>
+      </div>
 
-        <div class="col-lg-6">
-          <div data-anim-child="slide-up delay-6" class="masthead__image">
-            <img src="assets/images/slider/1.jpg" alt="image">
-          </div>
+      <div class="col-lg-6">
+        <div data-anim-child="slide-up delay-6" class="masthead__image">
+          <img src="assets/images/slider/1.jpg" alt="image">
         </div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
+
+<section data-anim-wrap class="mainSlider -type-1 js-mainSlider">
+  <div class="swiper-wrapper">
+
+    <div class="swiper-slide">
+      <div data-anim-child="fade" class="mainSlider__bg">
+        <div class="bg-image js-lazy" data-bg="admin/assets/img/landing.jpg"></div>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div data-anim-child="fade" class="mainSlider__bg">
+        <div class="bg-image js-lazy" data-bg="admin/assets/img/landing2.jpg"></div>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <div data-anim-child="fade" class="mainSlider__bg">
+        <div class="bg-image js-lazy" data-bg="admin/assets/img/landing3.jpg"></div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="container  pb-150">
+    <div class="row justify-center text-center">
+      <div class="col-xl-8 col-lg-10">
+        <div class="mainSlider__content">
+          <div data-anim-child="slide-up delay-2" class="text-17 lh-15 text-white fw-500 mb-10 subtxt">
+            AUTHENTIC || LUXURIOUS || AFFORDABLE
+          </div>
+          <h1 data-anim-child="slide-up delay-3" class="mainSlider__title text-white">
+            Experience the Perfect Blend of Style and Comfort with our
+            <span class="text-white underline">Premium Men’s Fabric</span>
+          </h1>
+
+          <p data-anim-child="slide-up delay-4" class="mainSlider__text text-white">
+          SENATOR MATERIAL | MEN’S FABRIC STORE IN LAGOS
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div data-anim-child="slide-up delay-6" class="row y-gap-20 justify-center mainSlider__items d-none">
+
+      <div class="col-xl-3 col-md-4 col-sm-6">
+        <div class="mainSlider-item text-center">
+          <img src="img/home-2/mainSlider/icons/1.svg" alt="icon">
+          <h4 class="text-20 fw-500 lh-18 text-white mt-8">100,000 online courses</h4>
+          <p class="text-15 text-white">Explore a variety of fresh topics</p>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-md-4 col-sm-6">
+        <div class="mainSlider-item text-center">
+          <img src="img/home-2/mainSlider/icons/2.svg" alt="icon">
+          <h4 class="text-20 fw-500 lh-18 text-white mt-8">Expert instruction</h4>
+          <p class="text-15 text-white">Find the right instructor for you</p>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-md-4 col-sm-6">
+        <div class="mainSlider-item text-center">
+          <img src="img/home-2/mainSlider/icons/3.svg" alt="icon">
+          <h4 class="text-20 fw-500 lh-18 text-white mt-8">Lifetime access</h4>
+          <p class="text-15 text-white">Learn on your schedule</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <button class="swiper-prev button -white-20 text-white size-60 rounded-full d-flex justify-center items-center js-prev">
+    <i class="icon icon-arrow-left text-24"></i>
+  </button>
+
+  <button class="swiper-next button -white-20 text-white size-60 rounded-full d-flex justify-center items-center js-next">
+    <i class="icon icon-arrow-right text-24"></i>
+  </button>
+</section>
