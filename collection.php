2213c1f4e501b62e6161b1a2953de8b292b1f2d4
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-03 11:27:04 +0100

    Fixing Store Data

--- collection.php
+++ collection.php
@@ -1,7 +1,11 @@
 <?php
 $pagetitle = "Collection";
 include_once "comp/head.php";
-include_once "comp/header.php"
+include_once "comp/header.php";
+
+
+
+
 ?>
 
 
@@ -242,38 +246,35 @@ include_once "comp/header.php"
 
 <section class="layout-pt-md layout-pb-lg border-top-light">
     <div data-anim-wrap class="container">
-       
+
         <div class=" js-section-slider" data-gap="30" data-pagination data-slider-cols="xl-3 lg-3 md-2">
             <div class="swiper-wraper row y-gap-30">
 
-              
+                <?php
+                $categories = mysqli_query($conn, "SELECT * FROM olnee_categories");
+                while ($row_categories = mysqli_fetch_assoc($categories)) {
+                    $categoryname = $row_categories['categoryName'];
+                    $category_id = $row_categories["categoryid"];
+                ?>
+
                 <div class="swiper-slid col-lg-3">
                     <div class=" bg-light-1 rounded-8 with-image-bg " style="background-image: url('admin/assets/img/landing.jpg');">
                         <div class="eventCard -type-3 rounded-8">
                             <div class="eventCard__date">
                                 <h3 class="text-30 lh-1 fw-500 ml-15 text-white">Senator</h3>
                             </div>
-
-
-
                             <div class="eventCard__button">
-
                                 <a href="#" class="button -icon -white text-deep-green-1">
                                     View Collection
                                     <i class="icon-arrow-top-right text-13 ml-10"></i>
                                 </a>
-
                             </div>
                         </div>
                     </div>
                 </div>
-
-             
-
-
+                <?php } ?>
+                
             </div>
-
-           
         </div>
     </div>
 </section>
