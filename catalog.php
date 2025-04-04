60c4e50ee56531ebb1fc4e6ba8a336838e86576e
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-04 11:50:29 +0100

    Added avialbilty functionality

--- catalog.php
+++ catalog.php
@@ -201,10 +201,8 @@ include_once "comp/header.php"
                         $prodsql_img_thumbnail = mysqli_query($conn, "SELECT * FROM product_images WHERE product_id = '$product_id' AND thumbnail = 1");
                         $row_prod_img_thumbnail = mysqli_fetch_assoc($prodsql_img_thumbnail);
 
-
                         // $image_path_thumbnail = 'admin/' . $row_prod_img_thumbnail['image_path'];
                         // $product_img = $image_path_thumbnail;
-
 
                         $image_path_thumbnail = $row_prod_img_thumbnail['image_path'];
                         if (empty($image_path_thumbnail)) {
