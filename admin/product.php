50701fd54b42392859b4cba8139df71f722bc819
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-04 11:19:20 +0100

    Added avialbilty functionality

--- admin/product.php
+++ admin/product.php
@@ -119,6 +119,9 @@ $fname = $row['fname'];
                     $prodsql_img_thumbnail = mysqli_query($conn, "SELECT * FROM product_images WHERE product_id = '$product_id' AND thumbnail = 1");
                     $row_prod_img_thumbnail = mysqli_fetch_assoc($prodsql_img_thumbnail);
                     $image_path_thumbnail =  $row_prod_img_thumbnail['image_path'];
+                    if(empty($image_path_thumbnail)){
+                      $image_path_thumbnail = "product-img/product.png";
+                    }
 
                     // Get the non-thumbnail images
                     $prodsql_img = mysqli_query($conn, "SELECT * FROM product_images WHERE product_id = '$product_id' AND thumbnail = 0");
