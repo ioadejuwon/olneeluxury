1d8d66999689de645800c8a6f17ca7fe8e14afbf
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-04 12:18:02 +0100

    Added avialbilty functionality

--- comp/tail.php
+++ comp/tail.php
@@ -9,6 +9,6 @@
   <script src="admin/assets/js/main.js"></script>
   <script src="admin/assets/js/script.js"></script>
   <script src="admin/api/mainapi.js"></script>
-  <script src="admin/api/maincart1.js"></script>
+  <script src="admin/api/maincart11.js"></script>
 </body>
 </html>
