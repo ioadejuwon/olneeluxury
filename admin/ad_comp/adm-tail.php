d3bc0a7b7e047d977a369dd8af96d6f9330fc908
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-07 13:23:01 +0100

    Added the order update stutus feature

--- admin/ad_comp/adm-tail.php
+++ admin/ad_comp/adm-tail.php
@@ -8,7 +8,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="assets/js/vendors.js"></script>
     <script src="assets/js/main.js"></script>
-    <script src="api/miniapi8.js"></script>
+    <script src="api/miniapi7.js"></script>
     <script src="assets/js/scripts13.js"></script>
 </body>
 </html>
