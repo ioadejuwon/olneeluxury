7b7bf023449899d12e935cc20a08eaabe44774da
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-07 22:31:49 +0100

    Added Customers' Cam functionality

---
+++ pictures.php
@@ -0,0 +1,44 @@
+<?php
+$pagetitle = "Customers' Cam";
+include_once "comp/head.php";
+include_once "comp/header.php"
+?>
+<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+
+<section class="page-header -type-1 mt-60 text-white">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="page-header__content">
+            <div class="row justify-center text-center">
+                <div class="col-auto">
+                    <div data-anim="slide-up delay-1">
+                        <h1 class="page-header__title text-white">
+                            Customers' Cam
+                        </h1>
+                    </div>
+                    <div data-anim="slide-up delay-2">
+                        <p class="page-header__text">
+                            Some Pictures from our customers wearing our products.
+                            <br> We are happy to see you in our products.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="layout-pt-md layout-pb-lg border-top-light">
+    <div data-anim-wrap class="container">
+        <div class="grid">
+            <div class="grid-sizer"></div>
+            <div class="grid-item"><img src="admin/assets/img/landing4.jpg" alt=""></div>
+        </div>
+    </div>
+</section>
+
+
+<?php
+include_once "comp/footer.php";
+include_once "comp/tail.php";
+?>
