6a7da9c070bbd3f07527cba3430adb6ace0a2cfe
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-07 23:20:27 +0100

    Added Customers' Cam functionality

--- comp/head.php
+++ comp/head.php
@@ -56,7 +56,7 @@ include_once "admin/inc/drc.php";
     <!-- Stylesheets -->
     <link rel="stylesheet" href="admin/assets/css/vendors.css">
     <link rel="stylesheet" href="admin/assets/css/main.css">
-    <link rel="stylesheet" href="admin/assets/css/styles.css">
+    <link rel="stylesheet" href="admin/assets/css/styles1.css">
     <link rel="stylesheet" href="admin/assets/css/bootstrap.css">
 
     <!-- Page loading scripts-->
