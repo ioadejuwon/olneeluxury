742d351623daa34701ac032c4cc9d39bd38c7f40
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-08 16:44:31 +0100

    Added Customers' Cam functionality

--- admin/ad_comp/adm-tail.php
+++ admin/ad_comp/adm-tail.php
@@ -8,7 +8,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="assets/js/vendors.js"></script>
     <script src="assets/js/main.js"></script>
-    <script src="api/miniapi.js"></script>
-    <script src="assets/js/scripts13.js"></script>
+    <script src="api/miniapi32.js"></script>
+    <script src="assets/js/scripts14.js"></script>
 </body>
 </html>
--- admin/assets/js/page-loader.js
+++ admin/assets/js/page-loader.js
@@ -1,3 +1,27 @@
+// console.log('api page is loaded');
+function showNotification(message, type = 'success') {
+  const container = document.getElementById('notification-container');
+  const notification = document.createElement('div');
+  notification.className = `notification ${type}`;
+  notification.textContent = message;
+  container.appendChild(notification);
+
+  // Show notification
+  setTimeout(() => {
+      notification.classList.add('show');
+  }, 100);
+
+  // Hide and remove notification after 3 seconds
+  setTimeout(() => {
+      notification.classList.remove('show');
+      setTimeout(() => {
+          container.removeChild(notification);
+      }, 500); // Match the transition duration
+  }, 3000);
+}
+
+
+
 function checkNetworkSpeed() {
   if ('connection' in navigator) {
     const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
