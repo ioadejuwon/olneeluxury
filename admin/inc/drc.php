f0a74194224f1eb4a88a4058322cb704de302e0c
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-10 23:38:33 +0100

    Added email fuctionality

--- admin/inc/drc.php
+++ admin/inc/drc.php
@@ -13,11 +13,11 @@ define('DOLLAR', '$');
 define('EURO', '€');
 
 // define('BRAND_EMAIL', 'hello@olneeluxury.com');
-define('REPLY_TO', 'admin@olneeluxury.com');
+define('REPLY_TO', 'admin@bioeconomyconf.com');
 define('COMPANY', 'Olnee Luxury');
 
 define('PHONE', '2348108806808');
-define('MAIL', 'admin@olneeluxury.com');
+define('MAIL', 'admin@bioeconomyconf.com');
 define('DEFAULT_IMG', 'product-img/product.png');
 define('CUSTOMERS_IMG_DIR', 'customers-cam/');
 define('PRODUCTS_IMG_DIR', 'product-img/');
