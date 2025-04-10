57e8375cc1c88d68db1d85c91978cce5f6ea9f36
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-10 21:06:58 +0100

    Added email fuctionality

--- admin/inc/sendmail.php
+++ admin/inc/sendmail.php
@@ -8,7 +8,7 @@ require '../vendor/autoload.php'; // Load PHPMailer
 require 'env.php'; // Load DRC
 
 
-function sendEmail($to, $toName, $subject, $htmlFile, &$response, $placeholders = [], $from = MAIL, $fromName = COMPANY, $replyTo = null, $cc = [], $bcc = [], $attachments = [])
+function sendEmail($to, $toName, $subject, $htmlFile, &$response, $placeholders = [], $from = 'admin@olneeluxury.com', $fromName = COMPANY, $replyTo = null, $cc = [], $bcc = [], $attachments = [])
 {
     $mail = new PHPMailer(true);
 
@@ -18,8 +18,8 @@ function sendEmail($to, $toName, $subject, $htmlFile, &$response, $placeholders 
         $mail->isSMTP();
         $mail->Host       = 'smtp.hostinger.com'; // Replace with your SMTP server
         $mail->SMTPAuth   = true;
-        $mail->Username   = MAIL; // Must match `setFrom`
-        $mail->Password   = EMAIL_PASSWORD; // SMTP password
+        $mail->Username   = 'admin@olneeluxury.com'; // Must match `setFrom`
+        $mail->Password   = 'S9[1Q$,ZYcwD!,/'; // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587; // 465 for SSL, 587 for TLS
 
