95f2f234bc379ca18896ef1c92ed127b3473335f
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2025-04-13 03:43:11 +0100

    Added email fuctionality

--- sendmail.php
+++ sendmail.php
@@ -5,7 +5,7 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php'; // Load PHPMailer
 // require 'drc.php'; // Load DRC
-// include_once 'admin/inc/env.php'; // Load DRC
+include_once 'admin/inc/env.php'; // Load DRC
 
 
 function sendEmail($to, $toName, $subject, $htmlFile, &$response, $placeholders = [], $from = MAIL, $fromName = COMPANY, $replyTo = null, $cc = [], $bcc = [], $attachments = [])
